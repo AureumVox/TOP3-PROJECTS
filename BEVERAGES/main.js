95d41002bed78b5bbc2fefa78c8aa6b962a80d96
musikera4 <kimsarmientoespiritu.a2b@gmail.com>
2018-12-03 07:03:23 +0800

    add

---
+++ BEVERAGES/main.js
@@ -0,0 +1,18 @@
+
+const sr = ScrollReveal({
+    duration: 2000,
+    reset:true
+});
+
+sr.reveal ('.btn', {
+    delay: 50
+})
+
+sr.reveal ('.col-lg-3', {
+    delay: 30
+})
+
+q$(".navbar-fixed-top").autoHidingNavbar({
+});
+
+

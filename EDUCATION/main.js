bd24557f8becf3befb7a36c421ddbdfa135f72aa
musikera4 <kimsarmientoespiritu.a2b@gmail.com>
2018-12-03 07:11:50 +0800

    Second

---
+++ EDUCATION/main.js
@@ -0,0 +1,27 @@
+
+const sr = ScrollReveal({
+    duration: 2000,
+    reset:true
+});
+
+sr.reveal ('.btn', {
+    delay: 50
+})
+
+sr.reveal ('.padding', {
+    delay: 30
+})
+
+sr.reveal ('.myCarousel', {
+    delay: 30
+})
+
+sr.reveal ('#landingpages', {
+    delay: 30
+})
+
+
+q$(".navbar-fixed-top").autoHidingNavbar({
+});
+
+
